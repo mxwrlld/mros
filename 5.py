a61e4e01a65673e56c8d169a806759d454e4bce6
mxwrlld <maksim.mixwell@gmail.com>
2022-12-14 14:24:22 +0400

    [5] Добавление KNN

--- 5.py
+++ 5.py
@@ -1,3 +1,4 @@
+import collections
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
@@ -49,14 +50,15 @@ def painter(title: str, xs: list, lonely: bool = True):
 
 
 def calc_parsen_kernel(x: np.ndarray, x_i: np.ndarray, B: np.ndarray, e_power_multiplier, const):
-    power =  e_power_multiplier * (x - x_i) @ np.linalg.inv(B) @ (x - x_i)
+    power = e_power_multiplier * (x - x_i) @ np.linalg.inv(B) @ (x - x_i)
     return const * np.exp(power)
+
 
 def parsen_classification(x, train_vectors_by_classes, Bs, count_of_classes, count_of_train_vectors, vcalc_parsen_kernel):
     f = np.zeros(count_of_classes)
     P = np.zeros(count_of_classes)
-    k = 0.3 
-    
+    k = 0.3
+
     for j in range(count_of_classes):
         vectors_by_class = train_vectors_by_classes[j]
         _class_dim = vectors_by_class.shape[1]  # -- N
@@ -65,11 +67,13 @@ def parsen_classification(x, train_vectors_by_classes, Bs, count_of_classes, cou
             (np.linalg.det(Bs[j]) ** (- 0.5))
         exp_sequence = []
         e_power_multiplier = (- 0.5 * (h ** (- 2)))
-        exp_sequence = vcalc_parsen_kernel(x, vectors_by_class.T, Bs[j], e_power_multiplier, const)
+        exp_sequence = vcalc_parsen_kernel(
+            x, vectors_by_class.T, Bs[j], e_power_multiplier, const)
         f[j] = np.average(exp_sequence)
         P[j] = _class_dim
 
     return np.argmax((P / count_of_train_vectors) * f)
+
 
 def parsen(train_vectors_by_classes: np.ndarray, test_sample: np.ndarray, Bs: np.ndarray):
     classification_res = np.ndarray(shape=test_sample.shape[1])
@@ -88,8 +92,38 @@ def parsen(train_vectors_by_classes: np.ndarray, test_sample: np.ndarray, Bs: np
         excluded=[1, 2]
     )
 
-    classification_res = vparsen_classification(test_sample.T, train_vectors_by_classes, Bs, count_of_classes, count_of_train_vectors, vcalc_parsen_kernel)
+    classification_res = vparsen_classification(
+        test_sample.T, train_vectors_by_classes, Bs, count_of_classes, count_of_train_vectors, vcalc_parsen_kernel)
 
+    return classification_res
+
+
+def calc_euclidean_distance(x: np.ndarray, y: np.ndarray):
+    return np.linalg.norm(x - y)
+
+
+def knn_classification(x, train_sample, train_labels, K, vcalc_distance):
+    distances = vcalc_distance(train_sample.T, x.T)
+    neighbors_indexes = distances.argpartition(K)[:K]
+    neighbors_classes = train_labels[neighbors_indexes]
+    return collections.Counter(neighbors_classes).most_common(1)[0][0]
+
+
+def k_nearest_neighbour(train_sample: np.ndarray, train_labels: np.ndarray, test_sample: np.ndarray, K: int):
+    classification_res = np.ndarray(shape=test_sample.shape[1])
+
+    vcalc_distance = np.vectorize(
+        calc_euclidean_distance,
+        signature='(n), (m) -> ()'
+    )
+    vknn_classification = np.vectorize(
+        knn_classification,
+        signature='(n), (), () -> ()',
+        excluded=[1, 2]
+    )
+
+    classification_res = vknn_classification(
+        test_sample.T, train_sample, train_labels, K, vcalc_distance)
     return classification_res
 
 
@@ -105,10 +139,10 @@ def sample_2_classified_sample(sample: np.ndarray, labels: np.ndarray) -> dict:
 def calc_errors(initial_vectors_by_classes, classified_vectors_by_classes):
     error_vectors = None
     vis_classif_error_vector = np.vectorize(
-            lambda initial_vector, classified_vectors: (
-                initial_vector == classified_vectors).any() == False,
-            signature='(n)->()',
-            excluded=[1]
+        lambda initial_vector, classified_vectors: (
+            initial_vector == classified_vectors).any() == False,
+        signature='(n)->()',
+        excluded=[1]
     )
 
     for _class in range(len(initial_vectors_by_classes)):
@@ -116,12 +150,13 @@ def calc_errors(initial_vectors_by_classes, classified_vectors_by_classes):
         classified_vectors_by_class = classified_vectors_by_classes[_class].T
 
         error_vectors_indexes = vis_classif_error_vector(initial_vectors_by_class,
-                                classified_vectors_by_class)
+                                                         classified_vectors_by_class)
         if error_vectors is None:
-            error_vectors = initial_vectors_by_classes[_class][:, error_vectors_indexes]
-        else: 
-            error_vectors = np.concatenate([error_vectors, initial_vectors_by_classes[_class][:, error_vectors_indexes]], axis=1)
-    
+            error_vectors = initial_vectors_by_classes[_class][:,
+                                                               error_vectors_indexes]
+        else:
+            error_vectors = np.concatenate(
+                [error_vectors, initial_vectors_by_classes[_class][:, error_vectors_indexes]], axis=1)
 
     return error_vectors
 
@@ -130,7 +165,7 @@ if __name__ == "__main__":
     config = {
         "generate": True,
         "save": False,
-        "checkpoints": [1]
+        "checkpoints": [2]
     }
 
     train_vectors = get_vectors(
@@ -181,7 +216,46 @@ if __name__ == "__main__":
         mng.window.state('zoomed')
         plt.legend()
         plt.show()
-        
+
+        # Оценка эмпирического риска
+        R = errors.shape[1] / (test_sample.shape[0] * N)
+        print(f"Оценка суммарной вероятности ошибочной классификации: ", R)
+
+    # ==================== К ближайших соседей  ==================== #
+    """
+    Построить классификатор, основанный на методе К ближайших соседей (для 
+    K=1,3,5), используя сгенерированные в п.1 данные как обучающие выборки, а 
+    данные из первой лабораторной работы - как тестовые. Оценить эмпирический 
+    риск - оценку суммарной вероятности ошибочной классификации
+    """
+    if 2 in config["checkpoints"]:
+        K = 3
+        test_sample = np.concatenate(test_vectors[2:], axis=1)
+        train_sample = np.concatenate(train_vectors[2:], axis=1)
+        train_labels = np.array([
+            _class
+            for _class in range(len(train_vectors[2:]))
+            for _ in range(train_vectors[2:][_class].shape[1])
+        ])
+        labels = k_nearest_neighbour(
+            train_sample, train_labels, test_sample, K)
+        vectors_by_classes = sample_2_classified_sample(test_sample, labels)
+        errors = calc_errors(test_vectors[2:], vectors_by_classes)
+
+        # ==================== Демонстрация классификации  ==================== #
+        fig = plt.figure()
+        fig.add_subplot(1, 2, 1)
+        painter("Тестовая выборка", test_vectors[2:], lonely=False)
+        fig.add_subplot(1, 2, 2)
+        painter(f"К ближайших соседей. K - {K}",
+                vectors_by_classes, lonely=False)
+        plt.scatter(errors[0, :], errors[1, :], facecolors='none', s=27,
+                    edgecolors='black', label="Неверная классификация", alpha=1, linewidths=1.5)
+        mng = plt.get_current_fig_manager()
+        mng.window.state('zoomed')
+        plt.legend()
+        plt.show()
+
         # Оценка эмпирического риска
         R = errors.shape[1] / (test_sample.shape[0] * N)
         print(f"Оценка суммарной вероятности ошибочной классификации: ", R)
